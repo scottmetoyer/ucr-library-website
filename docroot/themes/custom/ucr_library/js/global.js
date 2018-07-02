96987c9bea94db51cdbffc9cce9e2693a1d038d4
Scott Metoyer <scott.metoyer@ucr.edu>
2018-07-02 10:31:11 -0700

    Set up the mega width banner

--- docroot/themes/custom/ucr_library/js/global.js
+++ docroot/themes/custom/ucr_library/js/global.js
@@ -9,7 +9,20 @@
 
   Drupal.behaviors.ucr_library_subtheme = {
     attach: function (context, settings) {
+      $(document).ready(function () {
+        $(window).resize(function () {
+          if ($(window).width() >= 980) {
+            $(".navbar .dropdown-toggle").hover(function () {
+              $(this).parent().toggleClass("show");
+              $(this).parent().find(".dropdown-menu").toggleClass("show");
+            });
 
+            $(".navbar .dropdown-menu").mouseleave(function () {
+              $(this).removeClass("show");
+            });
+          }
+        });
+      });
     }
   };
 
