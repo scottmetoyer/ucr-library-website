cf87c7fbcff3b7ce590656f4503adb7b8e694eb4
Scott Metoyer <scott.metoyer@ucr.edu>
2018-08-30 09:51:20 -0700

    Temporarily removed the hover state from the menu while debugging

--- docroot/themes/custom/ucr_library/js/global.js
+++ docroot/themes/custom/ucr_library/js/global.js
@@ -4,5 +4,25 @@
  *
  */
 (function ($, Drupal) {
+
   'use strict';
+
+  Drupal.behaviors.ucr_library_subtheme = {
+    attach: function (context, settings) {
+      $(document).ready(function () {
+        /*
+        if ($(window).width() >= 980) {
+          $(".navbar .dropdown-toggle").hover(function () {
+            $(this).parent().toggleClass("show");
+            $(this).parent().find(".dropdown-menu").toggleClass("show");
+          });
+
+          $(".navbar .dropdown-menu").mouseleave(function () {
+            $(this).removeClass("show");
+          });
+        }*/
+      });
+    }
+  };
+
 })(jQuery, Drupal);
