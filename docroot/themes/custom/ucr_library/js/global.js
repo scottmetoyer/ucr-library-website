8bc21782e5b70e48b2201e6e2a4602047aa1beb1
Scott Metoyer <scott.metoyer@ucr.edu>
2018-08-30 09:20:19 -0700

    Removed extraneous Bootstrap 3 theme

--- docroot/themes/custom/ucr_library/js/global.js
+++ docroot/themes/custom/ucr_library/js/global.js
@@ -4,26 +4,5 @@
  *
  */
 (function ($, Drupal) {
-
   'use strict';
-
-  Drupal.behaviors.ucr_library_subtheme = {
-    attach: function (context, settings) {
-      $(document).ready(function () {
-        $(window).resize(function () {
-          if ($(window).width() >= 980) {
-            $(".navbar .dropdown-toggle").hover(function () {
-              $(this).parent().toggleClass("show");
-              $(this).parent().find(".dropdown-menu").toggleClass("show");
-            });
-
-            $(".navbar .dropdown-menu").mouseleave(function () {
-              $(this).removeClass("show");
-            });
-          }
-        });
-      });
-    }
-  };
-
 })(jQuery, Drupal);
