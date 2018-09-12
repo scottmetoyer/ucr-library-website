8fe71411f197ef451d4797b058b6feb0025ac23f
Scott Metoyer <scott.metoyer@ucr.edu>
2018-09-12 14:21:24 -0700

    Added quicktabs module

--- docroot/modules/custom/eventbrite/src/Controller/EventbriteController.php
+++ docroot/modules/custom/eventbrite/src/Controller/EventbriteController.php
@@ -16,11 +16,33 @@ class EventbriteController extends ControllerBase
      * @return array
      *   Return markup array.
      */
+    private function getEvents() {
+        $body;
+        $url = 'https://www.eventbriteapi.com/v3/users/me/?token=PVVY6TKJY7CRARG7NN7K';
+        $method = 'GET';
+        $options = [
+        'form_params' => [
+        ]
+        ];
+        
+        $client = \Drupal::httpClient();
+        
+        $response = $client->request($method, $url, $options);
+        $code = $response->getStatusCode();
+        if ($code == 200) {
+            $body = $response->getBody()->getContents();
+        }
+
+        return $body;
+    }
+
     public function content()
     {
+        $content = $this->getEvents();
+
         return [
             '#type' => 'markup',
-            '#markup' => $this->t('Hello, World!'),
+            '#markup' => $this->t($this->getEvents()),
         ];
     }
 
