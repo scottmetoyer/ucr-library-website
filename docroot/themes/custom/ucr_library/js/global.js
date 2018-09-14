77c1b692feb5cdc70c09e4420f9513cf30653485
Scott Metoyer <scott.metoyer@ucr.edu>
2018-09-14 06:38:41 -0700

    Made dropdown header links clickable

--- docroot/themes/custom/ucr_library/js/global.js
+++ docroot/themes/custom/ucr_library/js/global.js
@@ -10,17 +10,24 @@
   Drupal.behaviors.ucr_library_subtheme = {
     attach: function (context, settings) {
       $(document).ready(function () {
-        /*
-        if ($(window).width() >= 980) {
-          $(".navbar .dropdown-toggle").hover(function () {
-            $(this).parent().toggleClass("show");
-            $(this).parent().find(".dropdown-menu").toggleClass("show");
-          });
 
-          $(".navbar .dropdown-menu").mouseleave(function () {
-            $(this).removeClass("show");
-          });
-        }*/
+        // Make dropdown header links clickable
+        $('li.dropdown :first-child').once().on('click', function () {
+          var $el = $(this).parent();
+          var $a = $el.children('a.dropdown-toggle');
+          if ($a.length && $a.attr('href')) {
+            location.href = $a.attr('href');
+          }
+          // This code will make the header links work on a double click, or require the section to be open first.
+          /*
+          if ($el.hasClass('show')) {
+            var $a = $el.children('a.dropdown-toggle');
+            if ($a.length && $a.attr('href')) {
+              location.href = $a.attr('href');
+            }
+          }*/
+
+        });
       });
     }
   };
