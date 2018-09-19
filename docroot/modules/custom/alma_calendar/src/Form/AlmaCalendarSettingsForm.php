990378d0295e488ef093430910afaab5b9b34e86
Scott Metoyer <scott.metoyer@gmail.com>
2018-09-19 06:59:27 -0700

    Started working the Alma hours module

--- docroot/modules/custom/alma_calendar/src/Form/AlmaCalendarSettingsForm.php
+++ docroot/modules/custom/alma_calendar/src/Form/AlmaCalendarSettingsForm.php
@@ -35,17 +35,12 @@ class AlmaCalendarSettingsForm extends ConfigFormBase
     {
         $config = $this->config('alma_calendar.settings');
 
-        $form['oauth_token'] = array(
+        $form['api_key'] = array(
             '#type' => 'textfield',
-            '#title' => t('Personal OAuth token'),
-            '#default_value' => $config->get('oauth_token'),
-        );
+            '#title' => t('Alma API Key'),
+            '#default_value' => $config->get('api_key'),
+            '#description' => t('The Alma Application referenced by this API key must have Configuration endpoint permissions'),
 
-        $form['organization_id'] = array(
-            '#type' => 'textfield',
-            '#title' => t('Organization ID'),
-            '#default_value' => $config->get('organization_id'),
-            '#description' => t('This can be retrieved from the Eventbrite <a target="_blank" href="https://www.eventbriteapi.com/v3/users/me/organizations/?token='.$config->get('oauth_token').'">/users/me/organizations API endpoint</a>'),
         );
 
         return parent::buildForm($form, $form_state);
@@ -59,8 +54,7 @@ class AlmaCalendarSettingsForm extends ConfigFormBase
         $config = $this->config('alma_calendar.settings');
 
         // Set the submitted configuration setting
-        $config->set('oauth_token', $form_state->getValue('oauth_token'));
-        $config->set('organization_id', $form_state->getValue('organization_id'));
+        $config->set('api_key', $form_state->getValue('api_key'));
         $config->save();
 
         parent::submitForm($form, $form_state);
